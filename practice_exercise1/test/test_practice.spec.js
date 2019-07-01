43b0aac0a424e55526780e6eb6631154d8c3af51
Tien Le <tienle@mac.com>
2019-07-01 00:53:47 -0400

    Fix exercise 1 test to use function for describe and it

--- practice_exercise1/test/test_practice.spec.js
+++ practice_exercise1/test/test_practice.spec.js
@@ -1,42 +1,42 @@
 const assert = require('assert');
 const { callOneService, callTwoService, remoteMathService } = require('../practice.js');
 
-describe('Verify Service Call One', () => {
-  it('Should return a value of one', () => {
+describe('Verify Service Call One', function () {
+  it('Should return a value of one', function () {
     return callOneService((error, num) => num).then((results) => {
       assert.strictEqual(1, results);
     });
   });
 
-  it('Returned value should be error', () => {
+  it('Returned value should be error', function () {
     callOneService().catch((error) => {
       assert.strictEqual('Call back must be a function.', error.message);
     });
   });
 });
 
-describe('Verify Service Call Two', () => {
-  it('Should return a value of two', () => {
+describe('Verify Service Call Two', function () {
+  it('Should return a value of two', function () {
     return callTwoService((error, num) => num).then((results) => {
       assert.strictEqual(2, results);
     });
   });
 
-  it('Returned value should be error', () => {
+  it('Returned value should be error', function () {
     callTwoService().catch((error) => {
       assert.strictEqual('Call back must be a function.', error.message);
     });
   });
 });
 
-describe('Verify Remote Math Service', () => {
-  it('Returned value should be 3', () => {
+describe('Verify Remote Math Service', function () {
+  it('Returned value should be 3', function () {
     remoteMathService((error, value) => value).then((results) => {
       assert.strictEqual(3, results);
     });
   });
 
-  it('Returned value should be error', () => {
+  it('Returned value should be error', function () {
     remoteMathService().catch((error) => {
       assert.strictEqual('cb is not a function', error.message);
     });
